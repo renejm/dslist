cc911e5e08ddcb621bbc06805c05e0ed4da9e07b
Rene Juliano Martins <rene.j.martins@gmail.com>
2025-05-25 20:35:03 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -11,7 +11,7 @@ import com.devsuperior.dslist.projections.GameMinProjection;
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 	@Query(nativeQuery = true, value = """
-			SELECT g.id, g.title, g.game_year AS `year`, g.img_url AS imgUrl, g.short_description AS shortDescription, b.position
+			SELECT g.id, g.title, g.game_year AS gameYear, g.img_url AS imgUrl, g.short_description AS shortDescription, b.position
 			FROM tb_game g
 			INNER JOIN tb_belonging b ON g.id = b.game_id
 			WHERE b.list_id = :listId
